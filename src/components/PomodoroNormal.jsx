1f4add0e93d72a3b01e3d25aeddf0c17d5f9c110
meo0 <hashidamemos@gmail.com>
2024-08-27 04:06:10 +0900

    Style change: tailwindはたしかに良いがUIを作るとなるとだめ。なので、EmotionをベースにUIはMUI を使うのが現状、自分にあっていると思う

--- src/components/PomodoroNormal.jsx
+++ src/components/PomodoroNormal.jsx
@@ -1,5 +1,22 @@
-import { Switch } from '@headlessui/react';
+/** @jsxImportSource @emotion/react */
+import Switch from '@mui/material/Switch';
+import {css} from '@emotion/react';
 import {useState, useRef, useEffect} from 'react';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+
+import SettingsIcon from '@mui/icons-material/Settings';
 
 export const PomodoroNormal = () => {
     const [timef, setTimef] = useState(0);//seconds*1000
@@ -11,7 +28,8 @@ export const PomodoroNormal = () => {
     const workTime = 25;//minutes
     const breakTime = 5;//minutes
     const [isWorking, setIsWorking] = useState(true);
-    const [autoNext, setAutoNext] = useState(false);
+    const [autoNext, setAutoNext] = useState(true);
+    const [open, setOpen] = useState(false);
     useEffect(() => {
         if (isActive){
             startTimeRef.current = new Date();
@@ -21,11 +39,12 @@ export const PomodoroNormal = () => {
                 setTimef(toTimef);
                 const elapsedTime = Math.floor(toTimef / 1000);
                 setTime(elapsedTime);
-                if (isWorking && (elapsedTime >= workTime * 60 || elapsedTime >= breakTime * 60) ){
+                if (isWorking ? elapsedTime >= workTime * 60 : elapsedTime >= breakTime * 60){
+                    setTimef(0);
                     setTime(0);
                     setDuration(0);
                     setIsWorking(!isWorking);
-                    if (!autoNext) setIsActive(false);
+                    if (autoNext === false) setIsActive(false);
                 }
             }, 100);
         }else {
@@ -49,17 +68,19 @@ export const PomodoroNormal = () => {
             setIsActive(false);
         }
     };
+    const handleToggleStartPause = () => {
+        if(!isActive){
+            handleStart();
+        } else {
+            handlePause();
+        }
+    }
     const handleReset = () => {
         setIsActive(false);
+        setTimef(0);
         setTime(0);
         setDuration(0);
     };
-    const initSession = () => {
-        setTime(0);
-        setDuration(0);
-        setIsWorking(!isWorking);
-        if (!autoNext) setIsActive(false);
-    }
     const displayTime = (time) => {
         const minutes = Math.floor(time / 60);
         const seconds = time % 60;
@@ -70,10 +91,26 @@ export const PomodoroNormal = () => {
         return leftTime;
     }
     const handleToggleAutoNext = () => {
+        setDuration(timef);
         setAutoNext(!autoNext);
     }
+    const handleClickOpen = () => {
+        setOpen(true);
+    }
+    const handleClose = () => {
+        setOpen(false);
+    }
+    const settingStyle = css`
+            padding: 3px;
+            background-color: gray;
+            margin:1px 1px 1px auto;
+            `;
     return (
         <div>
+        <div css={css`display:flex;`}>
+        <IconButton css={settingStyle} size='large' aria-label='setting'><SettingsIcon/></IconButton>
+        </div>
+        <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
             <div
                 aria-hidden="true"
                 className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
@@ -86,27 +123,22 @@ export const PomodoroNormal = () => {
                 className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
                 />
             </div>
-            <p>{isWorking ? displayTime(countDownTime(time, workTime)) : displayTime(countDownTime(time, breakTime))}</p>
+            <div className='mx-auto max-w-2xl text-center'>
+            <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl p-4'>Pomodoro Timer</h2>
+            <p className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>{isWorking ? displayTime(countDownTime(time, workTime)) : displayTime(countDownTime(time, breakTime))}</p>
             <p className='text-gray-800 box'>{isWorking ? "Work time" : "Break Time"}</p>
-            <button onClick={handleStart} className={OperateButton}>Start</button>
-            <button onClick={handlePause} className={OperateButton}>Pause</button>
-            <button onClick={handleReset} className={OperateButton}>Reset</button>
-            <div className="flex items-center">
-            <p className='px-4'>Auto Start Next Tiemr </p>
-            <Switch
-                checked={autoNext}
-                onChange={handleToggleAutoNext}
-                className="group flex w-8 flex-none cursor-pointer rounded-full bg-gray-200 p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 data-[checked]:bg-indigo-600"
-              >
-                <span className="sr-only">Agree to policies</span>
-                <span
-                  aria-hidden="true"
-                  className="h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out group-data-[checked]:translate-x-3.5"
-                />
-            </Switch>
             </div>
+            <div className='text-center p-4'>
+            <button onClick={handleToggleStartPause} className={OperateButton}>{isActive ? 'Pause' : 'Start'}</button>
+            <button onClick={handleReset} className={OperateButton}>Reset</button>
+            </div>
+            <div className="flex items-center">
+            <p className='px-4'>Auto Start Next Timer </p>
+            <Switch checked={autoNext} onChange={handleToggleAutoNext} inputProps={{ 'aria-label': 'controlled' }}/>
+            </div>
+        </div>
         </div>
     );
 }
 
-const OperateButton = 'm-1 p-1 bg-blue-300 text-black rounded-md hover:bg-blue-700 hover:text-white';
+const OperateButton = 'm-1 p-1 bg-blue-300 text-black rounded-md hover:bg-blue-700 hover:text-white font-bold';
